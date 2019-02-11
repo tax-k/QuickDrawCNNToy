c82c26c3c6e7e5d79f962c332b92ba648147332a
tax_k <taxfree9090@gmail.com>
2019-02-11 18:36:54 +0900

    TEST commit from pycharm

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import cv2
+#import cv2
+
 import pandas as pd
 import numpy as np
+
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.neural_network import MLPClassifier
+
+# from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, roc_auc_score
+# from sklearn.model_selection import train_test_split, GridSearchCV
